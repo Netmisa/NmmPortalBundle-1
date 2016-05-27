4215c3391629913f1480b458274d9e5f13bd7864
Rémy Abi-Khalil <remyabi@gmail.com>
2016-05-27 15:50:03 +0200

    Symfony Mocker example

--- Features/Context/FeatureContext.php
+++ Features/Context/FeatureContext.php
@@ -3,12 +3,34 @@
 namespace CanalTP\NmmPortalBundle\Features\Context;
 
 use Behat\MinkExtension\Context\MinkContext;
+use PSS\Behat\Symfony2MockerExtension\ServiceMocker;
+use Behat\Symfony2Extension\Context\KernelDictionary;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends MinkContext
 {
+    use KernelDictionary;
+
+    /**
+     * @var ServiceMocker
+     */
+    private $serviceMocker;
+
+    /**
+     * @param ServiceMocker $serviceMocker
+     */
+    public function __construct(ServiceMocker $serviceMocker)
+    {
+        $this->serviceMocker = $serviceMocker;
+    }
+
+    public function setKernel($kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
     /**
      * @Given /^(?:|I )am logged in as Administrator$/
      */
@@ -18,5 +40,15 @@ class FeatureContext extends MinkContext
         $this->fillField('username', 'admin');
         $this->fillField('password', 'admin');
         $this->pressButton('Connexion');
+
+        $this->serviceMocker->mockService('canal_tp_tyr.api', 'CanalTP\TyrComponent\Guzzle3\TyrService')
+            ->shouldReceive('getUsers')
+            ->once()
+            ->andReturn("Hello World !")
+        ;
+        $container = $this->getContainer();
+        $users = $container->get('canal_tp_tyr.api')->getUsers();
+
+        dump($users);
     }
 }
